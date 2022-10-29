25c358a96414b079bcefe5399583b5615dd3516a
Theo Ward <theoward9@gmail.com>
2022-10-29 14:49:45 -0500

    updated problem with package, hello world home page

---
+++ Sub_to_Use/client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+
+    return(
+        <div>
+            <h1>
+                Hello World
+            </h1>
+        </div>
+    )
+};
+
+export default Home;
