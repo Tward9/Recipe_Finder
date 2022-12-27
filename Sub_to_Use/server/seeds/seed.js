aecd8573723425a44cf9f34a3ccb099b6cc926f9
Theo Ward <theoward9@gmail.com>
2022-12-27 15:08:17 -0600

    created instruciton seed

--- Sub_to_Use/server/seeds/seed.js
+++ Sub_to_Use/server/seeds/seed.js
@@ -1,15 +1,17 @@
-const { Recipe, Ingredient, toolSchema } = require('../models');
-const { recipeList, ingredientList, toolList } = require('../seeds')
+const { Recipe, Ingredient, Tool, Instruction } = require('../models');
+const { recipeList, ingredientList, toolList, instructionList } = require('../seeds')
 
 //create ingredients and recipes
 db.once('open', async () => {
     try {
         await Recipe.deleteMany({});
         await Ingredient.deleteMany({});
-        await toolSchema.deleteMany({});
+        await Tool.deleteMany({});
+        await Instruction.deleteMany({});
 
         await Ingredient.create(ingredientList);
-        await toolSchema.create(toolList);
+        await Tool.create(toolList);
+        await Instruction.create(instructionList);
         await Recipe.create(recipeList);
 
     } catch (err) {
