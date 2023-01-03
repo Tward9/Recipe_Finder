5b69cd291c5502a1dadfe49abe5c21191b45dfa3
Theo Ward <theoward9@gmail.com>
2023-01-03 15:11:30 -0500

    page holder for login, starting forms for login and sign up

---
+++ Sub_to_Use/client/src/pages/Login.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import LoginForm from '../components/LoginForm'
+import SignUpForm from "../components/SignUpForm";
+
+const Login = () => {
+
+    return(
+        <div>
+            
+        </div>
+    )
+};
+
+export default Login;
